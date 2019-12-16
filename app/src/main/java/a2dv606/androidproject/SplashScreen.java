090132ab645db4b705cb4fbd6a4e7a97d30222fe
Sukio99ns <dinhnamnghiem@gmail.com>
2019-12-16 22:34:13 +0700

    add splash activity

--- app/src/main/java/a2dv606/androidproject/SplashScreen.java
+++ app/src/main/java/a2dv606/androidproject/SplashScreen.java
@@ -18,7 +18,7 @@ public class SplashScreen extends Activity {
             public void run() {
 
                 try {
-                    sleep(3000);
+                    sleep(2000);
                     Intent splashIntent = new Intent (getApplicationContext(),MainActivity.class);
                     startActivity(splashIntent);
                     finish();
